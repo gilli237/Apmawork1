1093758775679d9c85062eacaead9603c5a2c1a2
Gildassobgoui <59330386+gilli237@users.noreply.github.com>
2021-01-05 07:58:06 +0100

    udate jan 5 morning

--- app/src/main/java/com/example/android/apma_work1/MainActivity.java
+++ app/src/main/java/com/example/android/apma_work1/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.android.apma_work1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView financeImgText;
+    LinearLayout financeLayout,cropLayout,animalLayout,productLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        financeLayout = findViewById(R.id.layoutFinance);
+        cropLayout = findViewById(R.id.layoutCrop);
+        animalLayout = findViewById(R.id.layoutAnimal);
+        productLayout = findViewById(R.id.layoutProducts);
+
+        financeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Loading.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+       cropLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Loading.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+       animalLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Loading.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        productLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Loading.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
+
+
 }
